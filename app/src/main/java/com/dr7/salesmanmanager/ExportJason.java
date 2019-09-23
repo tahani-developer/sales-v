e241eba09288ca9b705cc4cc2f0cfab0786ed2b6
Tahani Ghanoum <tahanighanoum1234@gmail.com>
2019-09-23 10:38:11 +0300

    export large json

--- app/src/main/java/com/dr7/salesmanmanager/ExportJason.java
+++ app/src/main/java/com/dr7/salesmanmanager/ExportJason.java
@@ -2,6 +2,7 @@ package com.dr7.salesmanmanager;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,14 +15,26 @@ import com.dr7.salesmanmanager.Modles.SalesManItemsBalance;
 import com.dr7.salesmanmanager.Modles.Transaction;
 import com.dr7.salesmanmanager.Modles.Voucher;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -139,93 +152,148 @@ public class ExportJason extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-            String JsonResponse = null;
-//            String JsonDATA = params[0];
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
 
-            try {
-                String ipAddress = mHandler.getAllSettings().get(0).getIpAddress(); // 10.0.0.115
-                String URL_TO_HIT = "http://" + ipAddress + "/VANSALES_WEB_SERVICE/index.php";
-
-                URL url = new URL(URL_TO_HIT);
-
-                String data = URLEncoder.encode("_ID", "UTF-8") + "=" +
-                        URLEncoder.encode(String.valueOf('2'), "UTF-8");
-
-               /* String table1 = data + "&" + "Sales_Voucher_M=" + jsonArrayVouchers.toString().trim();
-                String table2 = data + "&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim();
-                String table3 = data + "&" + "Payments="        + jsonArrayPayments.toString().trim();
-                String table4 = data + "&" + "Payments_Checks=" + jsonArrayPaymentsPaper.toString().trim();
-*/
-                String table1 = data + "&" + "Sales_Voucher_M=" + jsonArrayVouchers.toString().trim();
-                table1 +="&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim()
-                        // + "&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim()
-                        + "&" + "Payments="        + jsonArrayPayments.toString().trim()
-                        + "&" + "Payments_Checks=" + jsonArrayPaymentsPaper.toString().trim()
-                        + "&" + "Added_Customers=" + jsonArrayAddedCustomer.toString().trim()
-                        + "&" + "TABLE_TRANSACTIONS=" + jsonArrayTransactions.toString().trim()
-                        + "&" + "LOAD_VAN=" + jsonArrayBalance.toString().trim();//sales_man_item_balance
-
-//                URLConnection conn = url.openConnection();
-//
-//                conn.setDoOutput(true);
-//                conn.setDoInput(true);
+            String ipAddress = mHandler.getAllSettings().get(0).getIpAddress();
 
 
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setRequestMethod("POST");
 
-                try {
-//                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-//                    wr.write(table1);
-//
-//                    wr.flush();
+            try
+            {
 
-                    DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
-                    wr.writeBytes(table1);
-                    wr.flush();
-                    wr.close();
+                String JsonResponse = null;
+                HttpClient client = new DefaultHttpClient();
+                HttpPost  request = new HttpPost ();
+                request.setURI(new URI("http://" + ipAddress + "/VANSALES_WEB_SERVICE/index.php"));
 
-                } catch (Exception e){
-                    Log.e("here****" , e.getMessage());
-                }
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("_ID", "2"));
+                nameValuePairs.add(new BasicNameValuePair("Sales_Voucher_M", jsonArrayVouchers.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Sales_Voucher_D", jsonArrayItems.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Payments", jsonArrayPayments.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Payments_Checks", jsonArrayPaymentsPaper.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Added_Customers", jsonArrayAddedCustomer.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("TABLE_TRANSACTIONS", jsonArrayTransactions.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("LOAD_VAN", jsonArrayBalance.toString().trim()));
 
+                request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-                // get response
-                reader = new BufferedReader(new
-                        InputStreamReader(httpURLConnection.getInputStream()));
+                HttpResponse response = client.execute(request);
 
-                StringBuilder sb = new StringBuilder();
-                String line = null;
+                BufferedReader in = new BufferedReader(new
+                        InputStreamReader(response.getEntity().getContent()));
 
-                // Read Server Response
-                while((line = reader.readLine()) != null) {
+                StringBuffer sb = new StringBuffer("");
+                String line="";
+
+                while ((line = in.readLine()) != null) {
                     sb.append(line);
                 }
+
+                in.close();
+
 
                 JsonResponse = sb.toString();
                 Log.e("tag", "" + JsonResponse);
 
                 return JsonResponse;
 
-            } catch (IOException e) {
+
+            }catch (Exception e)
+            {
                 e.printStackTrace();
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e("tag", "Error closing stream", e);
-                    }
-                }
+                return null;
             }
-            return null;
+
+
+//            String JsonResponse = null;
+////            String JsonDATA = params[0];
+//            HttpURLConnection urlConnection = null;
+//            BufferedReader reader = null;
+//
+//            try {
+//                String ipAddress = mHandler.getAllSettings().get(0).getIpAddress(); // 10.0.0.115
+//                String URL_TO_HIT = "http://" + ipAddress + "/VANSALES_WEB_SERVICE/index.php";
+//
+//                URL url = new URL(URL_TO_HIT);
+//
+//                String data = URLEncoder.encode("_ID", "UTF-8") + "=" +
+//                        URLEncoder.encode(String.valueOf('2'), "UTF-8");
+//
+//               /* String table1 = data + "&" + "Sales_Voucher_M=" + jsonArrayVouchers.toString().trim();
+//                String table2 = data + "&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim();
+//                String table3 = data + "&" + "Payments="        + jsonArrayPayments.toString().trim();
+//                String table4 = data + "&" + "Payments_Checks=" + jsonArrayPaymentsPaper.toString().trim();
+//*/
+//                String table1 = data + "&" + "Sales_Voucher_M=" + jsonArrayVouchers.toString().trim();
+//                table1 +="&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim()
+//                        // + "&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim()
+//                        + "&" + "Payments="        + jsonArrayPayments.toString().trim()
+//                        + "&" + "Payments_Checks=" + jsonArrayPaymentsPaper.toString().trim()
+//                        + "&" + "Added_Customers=" + jsonArrayAddedCustomer.toString().trim()
+//                        + "&" + "TABLE_TRANSACTIONS=" + jsonArrayTransactions.toString().trim()
+//                        + "&" + "LOAD_VAN=" + jsonArrayBalance.toString().trim();//sales_man_item_balance
+//
+////                URLConnection conn = url.openConnection();
+////
+////                conn.setDoOutput(true);
+////                conn.setDoInput(true);
+//
+//
+//                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//                httpURLConnection.setDoOutput(true);
+//                httpURLConnection.setDoInput(true);
+//                httpURLConnection.setRequestMethod("POST");
+//
+//                try {
+////                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+////                    wr.write(table1);
+////
+////                    wr.flush();
+//
+//                    DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+//                    wr.writeBytes(table1);
+//                    wr.flush();
+//                    wr.close();
+//
+//                } catch (Exception e){
+//                    Log.e("here****" , e.getMessage());
+//                }
+//
+//
+//                // get response
+//                reader = new BufferedReader(new
+//                        InputStreamReader(httpURLConnection.getInputStream()));
+//
+//                StringBuilder sb = new StringBuilder();
+//                String line = null;
+//
+//                // Read Server Response
+//                while((line = reader.readLine()) != null) {
+//                    sb.append(line);
+//                }
+//
+//                JsonResponse = sb.toString();
+//                Log.e("tag", "" + JsonResponse);
+//
+//                return JsonResponse;
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                if (urlConnection != null) {
+//                    urlConnection.disconnect();
+//                }
+//                if (reader != null) {
+//                    try {
+//                        reader.close();
+//                    } catch (final IOException e) {
+//                        Log.e("tag", "Error closing stream", e);
+//                    }
+//                }
+//            }
+//            return null;
+//
+
         }
 
         @Override
