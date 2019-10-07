97fd50cdcfbfcb932f13f385ffbb8e612d3f4c16
Tahani Ghanoum <tahanighanoum1234@gmail.com>
2019-10-07 17:30:11 +0300

    re export data

--- app/src/main/java/com/dr7/salesmanmanager/DeExportJason.java
+++ app/src/main/java/com/dr7/salesmanmanager/DeExportJason.java
@@ -12,6 +12,13 @@ import com.dr7.salesmanmanager.Modles.Item;
 import com.dr7.salesmanmanager.Modles.Payment;
 import com.dr7.salesmanmanager.Modles.Voucher;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -20,6 +27,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -155,55 +164,102 @@ public class DeExportJason extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-            String JsonResponse = null;
+
 //            String JsonDATA = params[0];
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
             try {
                 String ipAddress = mHandler.getAllSettings().get(0).getIpAddress(); // 10.0.0.115
-                String URL_TO_HIT = "http://" + ipAddress + "/VANSALES_WEB_SERVICE/index.php";
+//                String URL_TO_HIT = "http://" + ipAddress + "/VANSALES_WEB_SERVICE/index.php";
+//
+//                URL url = new URL(URL_TO_HIT);
+//
+//                String data = URLEncoder.encode("_ID", "UTF-8") + "=" +
+//                        URLEncoder.encode(String.valueOf('2'), "UTF-8");
 
-                URL url = new URL(URL_TO_HIT);
 
-                String data = URLEncoder.encode("_ID", "UTF-8") + "=" +
-                        URLEncoder.encode(String.valueOf('2'), "UTF-8");
-
-
-                String table1 = data + "&" + "Sales_Voucher_M=" + jsonArrayVouchers.toString().trim();
-                table1 += "&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim()
-                        + "&" + "Payments=" + jsonArrayPayments.toString().trim()
-                        + "&" + "Payments_Checks=" + jsonArrayPaymentsPaper.toString().trim()
-                        + "&" + "added_customers=" + jsonArrayAddedCustomer.toString().trim();
-                URLConnection conn = url.openConnection();
-                conn.setDoOutput(true);
-                conn.setDoInput(true);
+                String JsonResponse = null;
+                HttpClient client = new DefaultHttpClient();
+                HttpPost request = new HttpPost ();
                 try {
-                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-                    wr.write(table1);
-
-                    wr.flush();
-                } catch (Exception e) {
-                    Log.e("here****", e.getMessage());
+                    request.setURI(new URI("http://" + ipAddress + "/VANSALES_WEB_SERVICE/index.php"));
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
                 }
 
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("_ID", "2"));
+                nameValuePairs.add(new BasicNameValuePair("Sales_Voucher_M", jsonArrayVouchers.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Sales_Voucher_D", jsonArrayItems.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Payments", jsonArrayPayments.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Payments_Checks", jsonArrayPaymentsPaper.toString().trim()));
+                nameValuePairs.add(new BasicNameValuePair("Added_Customers", jsonArrayAddedCustomer.toString().trim()));
+                request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-                // get response
-                reader = new BufferedReader(new
-                        InputStreamReader(conn.getInputStream()));
+                HttpResponse response = client.execute(request);
+                BufferedReader in = new BufferedReader(new
+                        InputStreamReader(response.getEntity().getContent()));
 
-                StringBuilder sb = new StringBuilder();
-                String line = null;
+                StringBuffer sb = new StringBuffer("");
+                String line="";
 
-                // Read Server Response
-                while ((line = reader.readLine()) != null) {
+                while ((line = in.readLine()) != null) {
                     sb.append(line);
                 }
+
+                in.close();
+
 
                 JsonResponse = sb.toString();
                 Log.e("tag", "" + JsonResponse);
 
                 return JsonResponse;
+
+
+
+
+
+
+
+
+
+
+//
+//                String table1 = data + "&" + "Sales_Voucher_M=" + jsonArrayVouchers.toString().trim();
+//                table1 += "&" + "Sales_Voucher_D=" + jsonArrayItems.toString().trim()
+//                        + "&" + "Payments=" + jsonArrayPayments.toString().trim()
+//                        + "&" + "Payments_Checks=" + jsonArrayPaymentsPaper.toString().trim()
+//                        + "&" + "added_customers=" + jsonArrayAddedCustomer.toString().trim();
+//                URLConnection conn = url.openConnection();
+//                conn.setDoOutput(true);
+//                conn.setDoInput(true);
+//                try {
+//                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+//                    wr.write(table1);
+//
+//                    wr.flush();
+//                } catch (Exception e) {
+//                    Log.e("here****", e.getMessage());
+//                }
+
+
+                // get response
+//                reader = new BufferedReader(new
+//                        InputStreamReader(conn.getInputStream()));
+//
+//                StringBuilder sb = new StringBuilder();
+//                String line = null;
+//
+//                // Read Server Response
+//                while ((line = reader.readLine()) != null) {
+//                    sb.append(line);
+//                }
+//
+//                JsonResponse = sb.toString();
+//                Log.e("tag", "" + JsonResponse);
+
+//                return JsonResponse;
 
             } catch (IOException e) {
                 e.printStackTrace();
